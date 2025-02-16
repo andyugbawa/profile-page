5dd015714912b60421fee3f50f6c9b633fa6bc0e
andy <andyugbawa@gmail.com>
2025-02-16 20:07:58 +0100

    new  changes

--- api/index.js
+++ api/index.js
@@ -5,7 +5,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/profileDB", {
+
+
+
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("Connected to MongoDB"))
